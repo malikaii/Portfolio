57e707b03b87c85c01eaa9ad37fe844bf407bb56
Malick Ouedraogo <C00400598@louisiana.edu>
2025-02-26 10:59:36 -0600

    Added base path to browserrouter

--- portfolio-malick/src/components/ErrorPage.jsx
+++ portfolio-malick/src/components/ErrorPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function ErrorPage() {
   return (
     <div>
         <h3>This page does not exist</h3>
+        <Link to={"/"}>Home</Link>
     </div>
   )
 }
