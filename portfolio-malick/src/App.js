d87cfe23abca5c7c021d43b7719749d2b6dc0073
Malick Ouedraogo <C00400598@louisiana.edu>
2025-02-26 11:08:23 -0600

    Removed base path to browserrouter

--- portfolio-malick/src/App.js
+++ portfolio-malick/src/App.js
@@ -8,7 +8,7 @@ import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
-    <BrowserRouter basename="https://malikaii.github.io/Portfolio/">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<ErrorPage />} />
